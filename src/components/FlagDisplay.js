3ca9c3ec7472fe9a81a052c9ab6e9d53d28e8ee2
Joe McColl <joe4kool@gmail.com>
2019-03-06 14:13:07 +0000

    added randomiser to flag display

--- src/components/FlagDisplay.js
+++ src/components/FlagDisplay.js
@@ -3,10 +3,13 @@ import React from 'react';
 const FlagDisplay = (props) => {
     
     const flagData = props.flags;
+    const randomFlag = flagData[Math.floor(Math.random()*flagData.length)];
+
+
     if (flagData.length === 0) return null;
     return ( 
 
-        <img src={flagData[0].flag} alt="country-name"></img>
+        <img src={randomFlag.flag} alt="country-name"></img>
         // console.log(flagData[0].name),
         //  null
      );
