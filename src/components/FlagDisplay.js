8d1f3478f2eebf54871e5ade6d4d85b5337cb29c
Joe McColl <joe4kool@gmail.com>
2019-03-06 14:18:38 +0000

    updated flag  css

--- src/components/FlagDisplay.js
+++ src/components/FlagDisplay.js
@@ -5,13 +5,11 @@ const FlagDisplay = (props) => {
     const flagData = props.flags;
     const randomFlag = flagData[Math.floor(Math.random()*flagData.length)];
 
-
     if (flagData.length === 0) return null;
     return ( 
-
+        <div className = "flag-display">
         <img src={randomFlag.flag} alt="country-name"></img>
-        // console.log(flagData[0].name),
-        //  null
+        </div>
      );
 }
  
