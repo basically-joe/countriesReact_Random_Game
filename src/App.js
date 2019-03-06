294a84a7d0cb3b5e217269d15fef1e3292f82dcd
Joe McColl <joe4kool@gmail.com>
2019-03-06 12:21:07 +0000

    added maincontainer, displaying text in browser

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import MainContainer from "./containers/MainContainer"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <h1>I'm the app.js page</h1>
+      <MainContainer/>
     );
   }
 }
---
+++ src/containers/MainContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <h1>I'm the main container!</h1> );
+    }
+}
+ 
+export default MainContainer;
