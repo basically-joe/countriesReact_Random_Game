68a1e9da1a22593a8a3eb50f817be8e5e8158138
Joe McColl <joe4kool@gmail.com>
2019-03-06 13:26:01 +0000

    added countires api, laoding in react dev

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react';
 class MainContainer extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            countries: []
+         }
     }
+
+componentDidMount(){
+    const url = "https://restcountries.eu/rest/v2/all"
+    fetch(url)
+    .then(res => res.json())
+    .then(data => this.setState({countries: data}))
+}
+
+
+
     render() { 
         return ( <h1>I'm the main container!</h1> );
     }
